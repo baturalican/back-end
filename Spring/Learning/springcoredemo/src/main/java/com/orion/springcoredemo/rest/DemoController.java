f3653f8c67061e05c562f8257ce5d96b60e49337
Ali Can Batur <baturalican@hotmail.com>
2024-04-17 02:21:01 +0200

    feat: spring learning add bean scopes and comment lazy evaluation

--- Spring/Learning/springcoredemo/src/main/java/com/orion/springcoredemo/rest/DemoController.java
+++ Spring/Learning/springcoredemo/src/main/java/com/orion/springcoredemo/rest/DemoController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach myCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach myCoach,
+                          @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = myCoach;
+        this.anotherCoach = anotherCoach;
     }
 
 //    @Autowired
@@ -24,6 +27,11 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 
 }
