7b12019843c3d981c5881f12212d1915e5f2c3e4
Ali Can Batur <baturalican@hotmail.com>
2024-08-14 19:04:10 +0200

    feat: spring learning read student data from MySQL DB using JPA

--- Spring/Learning/cruddemo/src/main/java/com/orion/cruddemo/CrudDemoApplication.java
+++ Spring/Learning/cruddemo/src/main/java/com/orion/cruddemo/CrudDemoApplication.java
@@ -17,11 +17,34 @@ public class CrudDemoApplication {
 		@Bean
 		public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 			return runner -> {
-				manageStudent(studentDAO);
+//				createStudent(studentDAO);
+//				createMultipleStudents(studentDAO);
+				readStudent(studentDAO);
 			};
 		}
 
-	private void manageStudent(StudentDAO studentDAO) {
+	private void readStudent(StudentDAO studentDAO) {
+		int id = 3;
+		// Read student with certain id
+		Student myStudent = studentDAO.findById(id);
+		System.out.println("Found the student: " + myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// Create multiple students
+		System.out.println("Creating 3 student objects...");
+		Student tempStudent1 = new Student("John", "Doe", "john@doe.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@public.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@applebum.com");
+
+		// Save the student objects to the database
+		System.out.println("Saving the students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
 		// Create student object
 		System.out.println("Creating new student object...");
 		Student tempStudent = new Student("Paul", "Doe", "paul@doe.com");
--- Spring/Learning/cruddemo/src/main/java/com/orion/cruddemo/dao/StudentDAO.java
+++ Spring/Learning/cruddemo/src/main/java/com/orion/cruddemo/dao/StudentDAO.java
@@ -4,4 +4,5 @@ import com.orion.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student student);
+    Student findById(Integer id);
 }
