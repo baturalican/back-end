20821087890bf93958b76309631831b105a5a93d
Ali Can Batur <baturalican@hotmail.com>
2025-08-01 01:19:07 +0200

    feat: spring learning add crud methods to employee service interface and implementation classes and delegate the calls to dao

--- Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/service/EmployeeServiceImpl.java
+++ Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.springboot.dbproject.dao.EmployeeDAO;
 import com.example.springboot.dbproject.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +20,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+        employeeDAO.delete(id);
     }
 }
