8ea76b0fae1366106690ecf8c7c26a516c5c7fd9
Ali Can Batur <baturalican@hotmail.com>
2025-09-10 13:38:11 +0200

    feat: spring learning add get employee by id functionality to the project

--- Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/rest/EmployeeRestController.java
+++ Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.example.springboot.dbproject.rest;
 import com.example.springboot.dbproject.entity.Employee;
 import com.example.springboot.dbproject.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id is not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
