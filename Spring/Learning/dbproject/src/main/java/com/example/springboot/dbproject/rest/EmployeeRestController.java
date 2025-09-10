b2d8204ad992bcd4e8a08b393e9bd6157dc0bfdc
Ali Can Batur <baturalican@hotmail.com>
2025-09-10 16:22:03 +0200

    feat: spring learning update existing employee functionality to the project

--- Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/rest/EmployeeRestController.java
+++ Spring/Learning/dbproject/src/main/java/com/example/springboot/dbproject/rest/EmployeeRestController.java
@@ -46,4 +46,12 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+
+    // Add mapping for /PUT employees, update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
 }
