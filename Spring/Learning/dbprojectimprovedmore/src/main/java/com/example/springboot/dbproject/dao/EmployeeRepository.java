9e574ad929dd827d0497a60010a67f3128b0ec9a
Ali Can Batur <baturalican@hotmail.com>
2025-10-16 15:38:29 +0200

    feat: spring learning add swagger ui and spring doc to db project

--- Spring/Learning/dbprojectimprovedmore/src/main/java/com/example/springboot/dbproject/dao/EmployeeRepository.java
+++ Spring/Learning/dbprojectimprovedmore/src/main/java/com/example/springboot/dbproject/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.springboot.dbproject.dao;
 
 import com.example.springboot.dbproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
