7462d43ccd0d6b153401bc3e4128bffa543fbecc
Ali Can Batur <baturalican@hotmail.com>
2026-04-14 14:37:26 +0200

    feat: spring learning add student form processing controller and fix show student form and fix for annotations

--- Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
+++ Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,9 +1,16 @@
 package com.example.springboot.thymeleafdemo.controller;
 
 import com.example.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class StudentController {
+
+    @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // Create a student object
         Student theStudent = new Student();
@@ -12,5 +19,11 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
     }
 }
