cc01c28bea2a4798f42826cd7a8778436fb96ab2
Ali Can Batur <baturalican@hotmail.com>
2026-04-29 18:29:49 +0200

    feat: spring learning add countries from a static properties file and fill dropbox with these values and display it.

--- Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
+++ Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.example.springboot.thymeleafdemo.controller;
 
 import com.example.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -17,6 +23,9 @@ public class StudentController {
 
         // Add student to the model
         theModel.addAttribute("student", theStudent);
+
+        // Add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
