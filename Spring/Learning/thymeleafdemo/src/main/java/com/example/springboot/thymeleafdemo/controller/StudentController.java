d47555645d7d3e4e19b76bed953692d8d68038ac
Ali Can Batur <baturalican@hotmail.com>
2026-05-04 17:45:42 +0200

    feat: spring learning add favorite programming language info from properties file rather than hardcoding them.

--- Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
+++ Spring/Learning/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
@@ -16,6 +16,9 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // Create a student object
@@ -26,6 +29,9 @@ public class StudentController {
 
         // Add the list of countries to the model
         theModel.addAttribute("countries", countries);
+
+        // Add the lost of programming languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
