d64bb649605ceed6d521b2e5a0f840c26aa50b40
Ali Can Batur <baturalican@hotmail.com>
2026-07-10 16:10:07 +0200

    feat: add spring learning customer confirmation trimming leading and trailing whitespaces

--- Spring/Learning/validationdemo/src/main/java/com/example/springdemo/mvc/CustomerController.java
+++ Spring/Learning/validationdemo/src/main/java/com/example/springdemo/mvc/CustomerController.java
@@ -1,15 +1,24 @@
 package com.example.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -22,6 +31,7 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
                                 BindingResult theBindingResult) {
+        System.out.println("Last name is |" + theCustomer.getLastName() + "|");
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         }
